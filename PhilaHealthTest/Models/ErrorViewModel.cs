fbed9fcfc3d4296c2a252ac1c60089616b4c9504
ENVYPS\psubi <psubianto@gmail.com>
2018-09-25 07:22:16 -0400

    New solution

---
+++ PhilaHealthTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhilaHealthTest.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
